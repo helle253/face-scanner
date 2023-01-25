24dd36dadf2e03d94bf188aa118f2cfcf5826fde
helle253 <nathanhellbhoy@gmail.com>
2023-01-25 17:18:28 -0600

    copy sample face detection app code

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,86 @@
+import 'package:arkit_plugin/arkit_plugin.dart';
 import 'package:flutter/material.dart';
+import 'package:vector_math/vector_math_64.dart' as vector;
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class FaceDetectionPage extends StatefulWidget {
+  const FaceDetectionPage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _FaceDetectionPageState createState() => _FaceDetectionPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _FaceDetectionPageState extends State<FaceDetectionPage> {
+  late ARKitController arkitController;
+  ARKitNode? node;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  ARKitNode? leftEye;
+  ARKitNode? rightEye;
+
+  @override
+  void dispose() {
+    arkitController.dispose();
+    super.dispose();
   }
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(title: const Text('Face Detection Sample')),
+        body: ARKitSceneView(
+          configuration: ARKitConfiguration.faceTracking,
+          onARKitViewCreated: onARKitViewCreated,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      );
+
+  void onARKitViewCreated(ARKitController arkitController) {
+    this.arkitController = arkitController;
+    this.arkitController.onAddNodeForAnchor = _handleAddAnchor;
+    this.arkitController.onUpdateNodeForAnchor = _handleUpdateAnchor;
+  }
+
+  void _handleAddAnchor(ARKitAnchor anchor) {
+    if (anchor is! ARKitFaceAnchor) {
+      return;
+    }
+    final material = ARKitMaterial(fillMode: ARKitFillMode.lines);
+    anchor.geometry.materials.value = [material];
+
+    node = ARKitNode(geometry: anchor.geometry);
+    arkitController.add(node!, parentNodeName: anchor.nodeName);
+
+    leftEye = _createEye(anchor.leftEyeTransform);
+    arkitController.add(leftEye!, parentNodeName: anchor.nodeName);
+    rightEye = _createEye(anchor.rightEyeTransform);
+    arkitController.add(rightEye!, parentNodeName: anchor.nodeName);
+  }
+
+  ARKitNode _createEye(Matrix4 transform) {
+    final position = vector.Vector3(
+      transform.getColumn(3).x,
+      transform.getColumn(3).y,
+      transform.getColumn(3).z,
     );
+    final material = ARKitMaterial(
+      diffuse: ARKitMaterialProperty.color(Colors.yellow),
+    );
+    final sphere = ARKitBox(
+        materials: [material], width: 0.03, height: 0.03, length: 0.03);
+
+    return ARKitNode(geometry: sphere, position: position);
+  }
+
+  void _handleUpdateAnchor(ARKitAnchor anchor) {
+    if (anchor is ARKitFaceAnchor && mounted) {
+      final faceAnchor = anchor;
+      arkitController.updateFaceGeometry(node!, anchor.identifier);
+      _updateEye(leftEye!, faceAnchor.leftEyeTransform,
+          faceAnchor.blendShapes['eyeBlink_L'] ?? 0);
+      _updateEye(rightEye!, faceAnchor.rightEyeTransform,
+          faceAnchor.blendShapes['eyeBlink_R'] ?? 0);
+    }
+  }
+
+  void _updateEye(ARKitNode node, Matrix4 transform, double blink) {
+    final scale = vector.Vector3(1, 1 - blink, 1);
+    node.scale = scale;
   }
 }
