1b6a849e37a742fa416fffce9ed48af3ec83aeb3
helle253 <nathanhellbhoy@gmail.com>
2023-01-25 20:34:00 -0600

    add main method

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,10 @@ import 'package:arkit_plugin/arkit_plugin.dart';
 import 'package:flutter/material.dart';
 import 'package:vector_math/vector_math_64.dart' as vector;
 
+void main() {
+  runApp(const FaceDetectionPage());
+}
+
 class FaceDetectionPage extends StatefulWidget {
   const FaceDetectionPage({Key? key}) : super(key: key);
 
